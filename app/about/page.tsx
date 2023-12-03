e1f6545e78dd66943dcfc726d45e8041023597ed
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-12-03 15:15:18 -0800

    update about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import AboutSection from "./AboutSection";
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return <AboutSection />;
 }
 
 export default AboutPage;
--- app/projects/ProjectsHeader.tsx
+++ app/projects/ProjectsHeader.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 export default function ProjectsHeader() {
   return (
-    <div className="py-12 sm:py-20 ">
+    <div className="md:w-10/12 py-12 sm:py-20 m-auto">
       {/* // projects header */}
       <h1 className="text-4xl sm:text-6xl inline-block w-auto mx-auto mb-8 relative">
         Projects
       </h1>
-      <p className="text-fun-gray text-xl sm:text-2xl max-w-3xl m-auto">
-        No todo lists or weather apps here. I apply my programming skills by
-        building things that I want to use.
-        <br />
+      <p className="pb-3 sm:text-lg m-auto">
+        No todo lists or weather apps here. I improve by building things I want
+        to use.
+      </p>
+      <p className="sm:text-lg m-auto">
         I'm currently working on my most extensive project, Hardpost, a social
         e-commerce platform for skateboarders .
       </p>
