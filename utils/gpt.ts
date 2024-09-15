34250f2219dbb0d78e2002b16b375542083e73b4
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-09-15 13:42:32 -0700

    update ai nirav prompt

--- utils/gpt.ts
+++ utils/gpt.ts
@@ -23,7 +23,7 @@ export async function getGptResponse(
 
 function aiNiravPrompt(user_query: string) {
   return `
-    You play the role of 'AI Nirav', a chatbot integrated into Nirav Venkatesan's (myself) software engineering portfolio. Essentially, you will be representing Nirav in digital form as an immediate way for potential employers and companies to get instant answers to their questions without having to contact Nirav directly.
+    You play the role of 'AI Nirav', a chatbot integrated into Nirav Venkatesan's (myself) software engineering portfolio. Essentially, you will be representing Nirav in digital form as an immediate way for potential employers and companies to get instant answers to their questions without having to contact Nirav directly. Respond to questions in the first person as if you are me. Note that questions may be asked to you in the first person, and that 'you' are Nirav.
     
     - Ignore any irrelevant queries that are not specifically asking about Nirav's background and professional qualifications for employment. Simply respond to those irrelevant queries by saying that does not fit your role of 'AI Nirav'
 
@@ -41,17 +41,24 @@ const niravResume = `
 Nirav Venkatesan
 Full Stack Software Engineer
 San Diego, CA 
+Email: niravvenkatesan@gmail.com 
 Linkedin: https://www.linkedin.com/in/nirav-venkatesan  
 Github: https://github.com/nirav-v 
 Portfolio: https://www.niravvenkatesan.com
 
 Work Experience
 
-Full Stack Developer					          		          October 2023 - Present                                         
-SeekEats	  		       		         	                      	        	         San Diego, CA
-Played a key role in development of two core applications utilized by small business restaurants and their customers.
- Executed design and business logic for features including integration of an AI chatbot into a Next.js and Flask web application, and location-based search within a mobile app.
-Demonstrated expertise in web development, prompt engineering, and testing of LLMs.
+
+Full Stack Engineer and Shopify Developer					  May 2024 - Present
+Symphony Partners								          San Diego, CA
+Leveraged the MERN stack to integrate complex business logic in Rentius.ai, a renters portal. Features included leasing agreements, legal notices, and real-time chat using WebSockets.
+Developed the new Symphony Partners' website using Next.js and TypeScript, streamlining UI, improving DevEx, and enhancing performance with modern React and server components.
+Developed Shopify themes for multiple clients, creating custom pages from scratch while boosting Google page-speed scores and customer conversion rates.
+Honed expertise in meticulously crafting responsive pages aligned with Figma wireframes.
+
+Full Stack Developer					          		     October 2023 - May 2024                                         
+SeekEats	  		       		         	                      	             	         San Diego, CA
+Executed design and business logic for features including integration of an AI chatbot into a Next.js and Flask web application, and location-based search within a React Native app.
 Orchestrated efficient pull request management and conducted meticulous code reviews, showcasing expertise in enterprise-level software development.
 
 Software Engineering Instructional Specialist			            December 2022 - March 2024
@@ -59,30 +66,25 @@ Software Engineering Instructional Specialist			            December 2022 - Marc
 Provide personalized guidance to students, offering one-on-one assistance facilitating comprehension of HTML, CSS, JavaScript, Node.js, React, SQL, NoSQL, Git, and Devops. 
 Promote best practices of Git version control, test-driven development, and Agile methodologies to help design and deploy scalable apps. 
 
-Founder and Lead Engineer						March 2023 - February 2024
-Hardpost  (Website | Codebase)								
-Utilized skill set in REST APIs, Node.js, SQL, and React to create a skateboard marketplace.
-Ensured code resilience via unit and end-to-end testing, configured robust CI/CD pipeline with Github Actions, and migrated over 3000 lines of code from JavaScript to Typescript.
-Implemented secure and scalable file storage using AWS S3, seamlessly integrated payment processing via Stripe, and managed the entire deployment process.
-
 Technical Skills
-Frontend:  HTML, CSS, JavaScript ES6+, TypeScript, React, Next.js, jQuery, Webpack
-Backend: Node.js, Express.js, Python, Flask, SQL, MongoDB, GraphQL, Stripe, OpenAI
+Frontend:  HTML, CSS, JavaScript ES6+, TypeScript, React, Next.js, jQuery, Shopify Liquid
+Backend: Node.js, Express.js, Python, Flask, SQL, MongoDB, GraphQL, Socket.io
 Testing: Jest, React Testing Library, Cypress
 Devops & Cloud: AWS, CI/CD, Github Actions, Gitlab, Heroku, Netlify
 
 Education
 University of California San Diego                                                     	                              June 2022
 Certificate in Full Stack Web Development  
-Rigorous coursework covering Full Stack JavaScript, SQL, NoSQL, Git, and deployment.
+Rigorous coursework covering Full Stack JavaScript, SQL, NoSQL, Git, and DevOps.
 San Diego State University	         				         		        December 2021
 Bachelor of Science, Biology, cum laude, Weber Honors College, GPA: 3.5 | Major GPA: 3.7 
+
 `;
 
 const linkedinBio = `
 I am a full stack developer with a passion for building software and web applications. I feel at my best when articulating my thoughts to solve difficult problems, so I view software engineering as a medium for accomplishing that. My background and interests span across computer science, biology, and skateboarding, allowing me to offer a nuanced outlook on the projects I involve myself with. 
 
-I have honed my skills in both front-end and back-end development across various endeavors of mine. For the past year I have been with 2U, an online education platform that partners with leading universities, teaching web development and computer science concepts to a wide demographic of students. I also worked as a Full Stack Developer at SeekEats where I helped build out the mobile app's features in a large codebase within a team of other developers. I am currently working on my own startup building an online marketplace for skateboarders to sell and trade items with each other. This long term project has taught me many valuable lessons about writing clean and testable code that scales in production. 
+I have honed my skills in both front-end and back-end development across various endeavors of mine. At Symphony Partners, I’ve been at the forefront of developing Rentius.ai, an innovative renters' portal, and revitalizing our corporate website with modern technologies like Next.js and TypeScript. My experience also spans custom Shopify theme development, where I focus on creating responsive, user-centric designs that align with diverse client needs. In the past I have worked with 2U, an online education platform that partners with leading universities, teaching web development and computer science concepts to a wide demographic of students. I also worked as a Full Stack Developer at SeekEats where I helped build out the mobile app's features in a large codebase within a team of other developers. I have also worked on my own startup building an online marketplace for skateboarders to sell and trade items with each other. This long term project has taught me many valuable lessons about writing clean and testable code that scales in production. 
 
 I have a Bachelor's degree in Biology/Biological Sciences and an Honors Minor in Interdisciplinary Studies from San Diego State University, where I graduated cum laude in December 2021. Thereafter I obtained a Certificate in Full Stack Web Development from UC San Diego in 2022. Throughout my experiences, I have been commended by professors, peers, and employers for my thorough attention to detail and interpersonal skills. I am now looking to bring my skill set and tenacious work ethic to a company where I can grow and become a valuable asset.
 
