f014c42af12b15c140274a0ffa75e51a785867ab
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-10-12 09:50:40 -0700

    add slide in animation to full page content rather than just header text

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,13 @@ export default function Home() {
     <>
       {" "}
       <div
-        className="relative w-full m-auto flex justify-center text-center flex-col items-center z-1"
+        className="animate-slideInLeft relative w-full m-auto flex justify-center text-center flex-col items-center z-1"
         style={{ maxWidth: "1200px" }}>
         <IntroTextHeader />
         <div className="p-4">
           <ChatInterface />
         </div>
+        <h2 className="m-auto text-2xl p-4">My Journey</h2>
         <ResumeComponent />
       </div>
     </>
