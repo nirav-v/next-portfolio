111157c085868ca14eb05a10d8873d7a5066c71e
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-10-12 11:09:10 -0700

    fix nested p tag on about pg error

--- app/about/AboutSection.tsx
+++ app/about/AboutSection.tsx
@@ -7,44 +7,43 @@ export default function AboutSection() {
         applications. I feel at my best when challenged to think and solve
         difficult problems, so I view software engineering as a medium for
         accomplishing that.
-        <p className="my-6 text-left">
-          Over the past 2 years, I have honed my skills in both front-end and
-          back-end development across various endeavors of mine. I am currently
-          building on an online e-commerce marketplace for skateboarders. While
-          it began just for personal learning, I now plan to scale the platform
-          to get real active users that can make transactions with it. This long
-          term project has taught me many valuable lessons about writing clean
-          and testable code that scales in production.
-        </p>
-        <p className="my-6"></p>I also love working collaboratively in fast
-        paced work environments, and have the experience to back it up. As a
-        Full Stack React Native Developer at SeekEats, I helped build out the
-        mobile app's features in a large codebase alongside various other
-        developers. I made pull requests, participated in code reviews, and also
-        implemented feedback from the design and product teams in order improve
-        the app's functionality and performance. In addition to developing apps
-        myself, I also teach web development and computer science concepts at
-        2U, an online education platform that partners with leading
-        universities. As a retained instructional specialist across 4 cohorts, I
-        facilitated student learning through class assignments and collaborative
-        projects that utilize Agile software development and Git version
-        control. I leverage my knowledge of various web technologies, such as
-        Express.js, SQL, MongoDB, HTML, CSS, REST APIs, and GraphQL, to
-        communicate with the class, answer questions, and problem solve. I am
-        responsible for ensuring that students are satisfied with the class
-        material, instructional support, and given the best resources to succeed
-        in the industry. My background spans from biology to skateboarding,
-        allowing me to offer both a scientific and creative outlook on the
-        projects I am involved with. I have a Bachelor's degree in
-        Biology/Biological Sciences and an Honors Minor in Interdisciplinary
-        Studies from San Diego State University, where I graduated cum laude in
-        December 2021. I also have a Certificate in Full Stack Web Development
-        from UC San Diego. Throughout my experiences, I have been commended by
-        professors, peers, and employers for my thorough attention to detail and
-        interpersonal skills. I am now looking to bring my skill set and
-        tenacious work ethic to a company where I can grow and become a valuable
-        asset.
       </p>
+      <p className="my-6 text-left">
+        Over the past 2 years, I have honed my skills in both front-end and
+        back-end development across various endeavors of mine. I am currently
+        building on an online e-commerce marketplace for skateboarders. While it
+        began just for personal learning, I now plan to scale the platform to
+        get real active users that can make transactions with it. This long term
+        project has taught me many valuable lessons about writing clean and
+        testable code that scales in production.
+      </p>
+      <p className="my-6"></p>I also love working collaboratively in fast paced
+      work environments, and have the experience to back it up. As a Full Stack
+      React Native Developer at SeekEats, I helped build out the mobile app's
+      features in a large codebase alongside various other developers. I made
+      pull requests, participated in code reviews, and also implemented feedback
+      from the design and product teams in order improve the app's functionality
+      and performance. In addition to developing apps myself, I also teach web
+      development and computer science concepts at 2U, an online education
+      platform that partners with leading universities. As a retained
+      instructional specialist across 4 cohorts, I facilitated student learning
+      through class assignments and collaborative projects that utilize Agile
+      software development and Git version control. I leverage my knowledge of
+      various web technologies, such as Express.js, SQL, MongoDB, HTML, CSS,
+      REST APIs, and GraphQL, to communicate with the class, answer questions,
+      and problem solve. I am responsible for ensuring that students are
+      satisfied with the class material, instructional support, and given the
+      best resources to succeed in the industry. My background spans from
+      biology to skateboarding, allowing me to offer both a scientific and
+      creative outlook on the projects I am involved with. I have a Bachelor's
+      degree in Biology/Biological Sciences and an Honors Minor in
+      Interdisciplinary Studies from San Diego State University, where I
+      graduated cum laude in December 2021. I also have a Certificate in Full
+      Stack Web Development from UC San Diego. Throughout my experiences, I have
+      been commended by professors, peers, and employers for my thorough
+      attention to detail and interpersonal skills. I am now looking to bring my
+      skill set and tenacious work ethic to a company where I can grow and
+      become a valuable asset.
     </div>
   );
 }
