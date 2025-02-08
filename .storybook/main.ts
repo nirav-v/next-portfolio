19133185ed800f65a76d3fcdfab40280704cb690
Nirav Venkatesan <niravvenkatesan@gmail.com>
2025-02-08 14:40:50 -0800

    storybook setup

---
+++ .storybook/main.ts
@@ -0,0 +1,11 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: [
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  framework: '@storybook/nextjs',
+  staticDirs: ['../public'],
+};
+export default config;
