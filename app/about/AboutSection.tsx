74764379345438c9206a8874a74e8e37163379a5
Nirav Venkatesan <niravvenkatesan@gmail.com>
2025-02-15 18:00:27 -0800

    fix about alignment

--- app/about/AboutSection.tsx
+++ app/about/AboutSection.tsx
@@ -3,7 +3,7 @@ import AboutMd from './markdown/about.mdx';
 
 export default function AboutSection() {
   return (
-    <div className='p-8 m-auto md:w-8/12'>
+    <div className='p-8 m-auto md:w-8/12 flex flex-col items-center'>
       <h1 className='font-bold text-2xl p-4 text-slate-100'>About Me</h1>
       <div className='text-left prose prose-slate'>
         <AboutMd />
