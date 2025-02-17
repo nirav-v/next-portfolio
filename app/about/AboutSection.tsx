15c54200a7d4194d9cd319e801205ff064fa3934
Nirav Venkatesan <niravvenkatesan@gmail.com>
2025-02-17 08:58:33 -0800

    consolidate an addtional markdown styling to be inline in mdx componenet

--- app/about/AboutSection.tsx
+++ app/about/AboutSection.tsx
@@ -3,11 +3,10 @@ import AboutMd from './markdown/about.mdx';
 
 export default function AboutSection() {
   return (
-    <div className='p-8 m-auto md:w-8/12 flex flex-col items-center'>
+    <div className='p-8 m-auto md:w-10/12 flex flex-col items-center'>
       <h1 className='font-bold text-2xl p-4 text-slate-100'>About Me</h1>
-      <div className='text-left prose prose-slate'>
-        <AboutMd />
-      </div>
+
+      <AboutMd />
     </div>
   );
 }
