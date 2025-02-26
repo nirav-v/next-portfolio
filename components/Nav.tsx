df6c2cc4e4d059716ac6831fdc0005214cc2cc50
Nirav Venkatesan <niravvenkatesan@gmail.com>
2025-02-25 16:59:37 -0800

    cleanup unused nav code

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
@@ -101,52 +101,5 @@ export default function Nav() {
         </nav>{' '}
       </div>
     </>
-  );
-}
-
-function MenuIcon(props: any) {
-  return (
-    <svg
-      className='h-5 w-5 absolute text-gray-100'
-      width='20'
-      height='20'
-      viewBox='0 0 20 20'
-      fill='none'
-      {...props}>
-      <path
-        d='M2.5 7.5H17.5'
-        stroke='currentColor'
-        strokeWidth='1.5'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-      <path
-        d='M2.5 12.5H17.5'
-        stroke='currentColor'
-        strokeWidth='1.5'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-    </svg>
-  );
-}
-
-function CrossIcon(props: any) {
-  return (
-    <svg
-      className='h-5 w-5 absolute text-gray-100'
-      viewBox='0 0 24 24'
-      width='24'
-      height='24'
-      stroke='currentColor'
-      strokeWidth='1.5'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      fill='none'
-      shapeRendering='geometricPrecision'
-      {...props}>
-      <path d='M18 6L6 18' />
-      <path d='M6 6l12 12' />
-    </svg>
   );
 }
